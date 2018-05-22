8b3e1c9170279e9b3e973e548edce17eebbcead5
PavelKovalchuk <kovpavanat@gmail.com>
2018-05-22 23:31:20 +0300

    basic component

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -23,11 +23,11 @@ class App extends Component {
 
     };
 
-    switchNameHandler = () => {
+    switchNameHandler = (newName) => {
        this.setState({
            persons: [
                {
-                   name: 'Max',
+                   name: newName,
                    age: 28,
                },
                {
@@ -42,14 +42,64 @@ class App extends Component {
        });
     }
 
+    nameChangeHandler = (event) => {
+
+        this.setState({
+            persons: [
+                {
+                    name: event.target.value,
+                    age: 28,
+                },
+                {
+                    name: event.target.value,
+                    age: 23,
+                },
+                {
+                    name: 'Stef',
+                    age: 18,
+                },
+            ],
+        });
+
+    }
+
     render() {
+
+        const style = {
+          backgroundColor: 'green',
+          border: '1px solid brown',
+          padding: '8px',
+          color: 'white',
+          cursor: 'pointer',
+        };
+
         return (
             <div className="App">
                 <h1 className="App-title">Hi I am React.</h1>
-                <button onClick={ this.switchNameHandler } >Switch name</button>
-                <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My hobbies: Racing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+                <button
+                    onClick={ () => this.switchNameHandler('Coming soon!') }
+                    style={style}
+                >
+                    Switch name
+                </button>
+
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age} />
+
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click = {this.switchNameHandler.bind(this, 'Pavel')}
+                    changed = {this.nameChangeHandler}
+                >
+                    My hobbies: Racing
+                </Person>
+
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age} />
+
             </div>
         );
     }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './Person.css';
 
 const person = (props) => {
 
     console.log('props: ', props);
     return (
-        <div>
+        <div className="Person">
 
-            <p>I am {props.name}. I am { props.age } years old.</p>
+            <p onClick={props.click}  >I am {props.name}. I am { props.age } years old.</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
 
     );
