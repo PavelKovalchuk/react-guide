742db762bb0c6f564a4e45f728e68f9c7a8ae28e
PavelKovalchuk <kovpavanat@gmail.com>
2018-05-30 14:00:53 +0300

    modal order

--- burger-shop/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-shop/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -25,7 +27,7 @@ class BurgerBuilder extends Component{
     }
 
     updatePurchaseState (ingredients){
-      
+
         const sum = Object.keys(ingredients)
             .map( igKey => {
                 return ingredients[igKey]
@@ -107,19 +109,18 @@ class BurgerBuilder extends Component{
 
             <Aux>
 
-                <div>
-                   <Burger ingredients={this.state.ingredients} />
-                </div>
-
-                <div>
-                    <BuildControls
+                <Modal>
+                    <OrderSummary ingredients={this.state.ingredients} />
+                </Modal>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
                         ingredientAdded = {this.addIngredientHandler}
                         ingredientRemoved = {this.removeIngredientHandler}
                         disabled = {disabledInfo}
                         price = {this.state.totalPrice}
                         purchasable = {this.state.purchasable}
-                    />
-                </div>
+                />
+
 
             </Aux>
         );
