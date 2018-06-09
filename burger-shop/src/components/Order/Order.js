252773ffb7ca6ba8ab8a0ba86f0c540c85b66136
PavelKovalchuk <kovpavanat@gmail.com>
2018-06-09 15:01:32 +0300

    burger orders finish

--- burger-shop/src/components/Order/Order.js
+++ burger-shop/src/components/Order/Order.js
@@ -3,11 +3,41 @@ import classes from './Order.css';
 
 const Order = (props) => {
 
+    const ingredients = [];
+
+    for(let ingredientName in props.ingredients ){
+
+        ingredients.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName],
+        });
+
+    }
+
+    const ingredientsOutput = ingredients.map(
+        ig => {
+            return(
+              <span
+                  key={ig.name}
+                  style = {{
+                      textTransform: 'capitalize',
+                      display: 'inline-block',
+                      margin: '0 8px',
+                      border: '1px solid #ccc',
+                      padding: '5px',
+                  }}
+              >
+                  {ig.name}: ({ig.amount})
+              </span>
+            );
+        }
+    );
+
     return(
       <div className={classes.Order} >
 
-          <p>Ingredients:</p>
-          <p>Price: USD 5</p>
+          <p>Ingredients: {ingredientsOutput} </p>
+          <p>Price: USD { Number.parseFloat(props.price).toFixed(2)}</p>
 
       </div>
     );
