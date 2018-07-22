bcf583d258ec9cde22a9270253ef5f6c82868b4c
PavelKovalchuk <kovpavanat@gmail.com>
2018-07-22 13:33:35 +0300

    auth adding to redux

---
+++ burger-shop/src/store/actions/auth.js
@@ -0,0 +1,38 @@
+import * as actionTypes from './actionTypes';
+
+//Action creator for authentication
+
+export const authStart = () => {
+
+    return {
+        type: actionTypes.AUTH_START,
+    };
+
+};
+
+export const authSuccess = (authData) => {
+
+    return {
+      type: actionTypes.AUTH_SUCCESS,
+      authData: authData,
+    };
+
+};
+
+export const authFail = (error) => {
+
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error,
+    };
+
+};
+
+//Async code. Thanks for Thunk
+export const auth = () => {
+
+    return dispatch => {
+        dispatch(authStart());
+    };
+
+};
