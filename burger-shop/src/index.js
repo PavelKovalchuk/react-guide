2244c00db058563d83c17e6e08325d61b210f9c1
PavelKovalchuk <kovpavanat@gmail.com>
2018-07-23 16:50:13 +0300

    using env variables

--- burger-shop/src/index.js
+++ burger-shop/src/index.js
@@ -13,7 +13,11 @@ import orderReducer from './store/reducers/order';
 import authReducer from './store/reducers/auth';
 
 //https://github.com/zalmoxisus/redux-devtools-extension
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development'
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null
+    || compose;
+
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
