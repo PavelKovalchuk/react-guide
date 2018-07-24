ce8f0bea22a0efd6e7fcae6731dcf1c6071cc0c8
PavelKovalchuk <kovpavanat@gmail.com>
2018-07-24 10:01:14 +0300

    testing - basic test 2

--- burger-shop/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-shop/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -25,4 +25,9 @@ describe('NavigationItems', () => {
         wrapper.setProps({isAuthenticated: true});
         expect(wrapper.find(NavigationItem)).toHaveLength(3);
     });
+
+    it('should have logout <NavigationItem/> if authenticated', () => {
+        wrapper.setProps({isAuthenticated: true});
+        expect(wrapper.contains(<NavigationItem link="/logout" >Logout</NavigationItem>)).toEqual(true);
+    });
 });
