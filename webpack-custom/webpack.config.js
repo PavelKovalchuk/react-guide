e5a56ffb48ee9619cc611f2cbb01db1c9f52a8c3
PavelKovalchuk <kovpavanat@gmail.com>
2018-07-24 14:14:30 +0300

    webpack added images config

--- webpack-custom/webpack.config.js
+++ webpack-custom/webpack.config.js
@@ -50,6 +50,10 @@ module.export = {
                     }
                 ],
                 exclude: /node_modules/
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/,
+                loader: 'url-loader?limit=8000&name=images/[name].[ext]'
             }
         ]
     }
