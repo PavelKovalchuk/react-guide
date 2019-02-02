a2cbf347bbabaaa7a338b3746da8066a3721a247
PavelKovalchuk <kovpavanat@gmail.com>
2019-02-02 16:24:06 +0200

    react hooks - Using Custom Hooks 2

--- react-hooks/src/App.js
+++ react-hooks/src/App.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react';
 import {LightBulb} from "./components/LightBulb";
 import {LightBulbContextContainer} from "./components/LightBulbContextContainer";
 import {Form} from "./components/Form";
+import {Counter} from "./components/Counter";
 
 class App extends Component {
   render() {
     return (
-      <LightBulbContextContainer>
-        <Form />
-        <LightBulb />
-      </LightBulbContextContainer>
+      <div style={{textAlign: "left", width: "80%", margin: 'auto'}}>
+        <LightBulbContextContainer>
+          <Counter />
+          <Form />
+          <LightBulb />
+        </LightBulbContextContainer>
+      </div>
     );
   }
 }
