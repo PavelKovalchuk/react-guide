ceff18961a58cbd49b4c9fc8e4af5435ba826b8f
PavelKovalchuk <kovpavanat@gmail.com>
2019-02-02 10:46:06 +0200

    react hooks - init

---
+++ react-hooks/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import {LightBulb} from "./components/LightBulb";
+
+class App extends Component {
+  render() {
+    return (
+      <LightBulb />
+    );
+  }
+}
+
+export default App;
